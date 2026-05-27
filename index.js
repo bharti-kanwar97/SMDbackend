7e482b0a0cd5b1abb14d37fd665ad145d8f15d89
bharti-kanwar97 <kanwarbharti96@gmail.com>
2026-05-27 12:36:57 +0530

    changes in index file

--- index.js
+++ index.js
@@ -20,12 +20,7 @@ const __dirname = path.dirname(__filename);
 // middleware
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
-app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
-}));
+app.use(cors());
 
 
 
